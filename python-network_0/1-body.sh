aeee0c6daaa36e5623a2efbaef1198e0901004f1
WamalwaSydney <s.wamalwa@alustudent.com>
2025-04-03 21:09:04 -0700

    marioo12

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display body only for status code 200
-[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" = "200" ] && curl -sL "$1"
+# Follow redirects and display body of 200 response
+curl -sL "$1"
