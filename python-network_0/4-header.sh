06322532147dd0d935f80ae8ec0b26149e260066
WamalwaSydney <s.wamalwa@alustudent.com>
2025-04-03 21:12:47 -0700

    marioo123

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a GET request with a specific header
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Send GET request with required header
+curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
