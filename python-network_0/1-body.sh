212f22b741d52f27f2a483717019e1dee18f0ecb
WamalwaSydney <s.wamalwa@alustudent.com>
2025-04-03 21:05:41 -0700

    marioo1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Fetch and display the body of a response if HTTP status code is 200
-curl -sL "$1" -o tmp_response && [ "$(tail -c 3 tmp_response)" = "200" ] && head -c -3 tmp_response && echo ""
+# Display body only for status code 200
+[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" = "200" ] && curl -sL "$1"
