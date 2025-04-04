4b5681b817eb0f74091726c14d64454147ec34d8
WamalwaSydney <s.wamalwa@alustudent.com>
2025-04-03 21:20:57 -0700

    marioo123456

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send GET request with required header
-curl -sL -H "X-HolbertonSchool-User-Id: 98" -H "Content-Type: application/json" "$1"
+# Send GET request with required header and force response check
+curl -sL -H "X-HolbertonSchool-User-Id: 98" -H "Accept: */*" "$1"
